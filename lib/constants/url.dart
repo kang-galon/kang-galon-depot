ebffe5c07bd68924f0eabeeca0cd7cd2efe85028
zalviandyr <zukronalviandy@gmail.com>
2021-06-15 14:32:19 +0700

    change url

--- lib/constants/url.dart
+++ lib/constants/url.dart
@@ -1,3 +1,4 @@
-const String _baseUrl = 'http://192.168.56.1:9000';
+// const String _baseUrl = 'http://192.168.56.1:9000';
+const String _baseUrl = 'https://kang-galon.herokuapp.com';
 
 Uri getUrl(String path) => Uri.parse(_baseUrl + path);
